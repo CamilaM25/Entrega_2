e0b14266bbaa627701246e6f4c8b60525e4c3b14
sher <sherlyn1620@gmail.com>
2024-11-04 00:04:04 -0500

    delete(pycache) eliminacion de datos innecesarios

--- Hospital/hce/urls.py
+++ Hospital/hce/urls.py
@@ -7,15 +7,12 @@ urlpatterns = [
     path('',views.inicio,name='inicio'),
     path('Equipos/ingresar',views.ingresar,name='ingresar'),
     path('Equipos',views.Equipos,name='Equipos'),
-<<<<<<< HEAD
     path('Equipos/editar/<int:id>',views.editar,name='editar'),
     path('borrar/<int:id>' , views.borrar, name='borrar'),
-=======
     path('Equipos/editar/<int:id>/',views.editar,name='editar'),
     path('borrar/<int:id>/' , views.borrar, name='borrar'),
     path('Responsables' , views.Responsables, name='Responsables'),
     path('Responsables/crear' , views.Responsablescrear, name='Responsablescrear'),
 
->>>>>>> 43533a4d6a6bbfedd651869e8c17416edb4ff97a
 
 ]
