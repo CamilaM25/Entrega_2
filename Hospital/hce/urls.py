279cd8b02545908692238cf10b60d5ad0d95e3f4
alexandraflorez1 <alexandra.florez1@udea.edu.co>
2024-11-08 23:01:37 -0500

    CRUD responsables terminado

--- Hospital/hce/urls.py
+++ Hospital/hce/urls.py
@@ -8,15 +8,20 @@ urlpatterns = [
     path('Equipos/equipo',views.equipo,name='equipo'),
     path('Responsables/responsable',views.responsable,name='responsable'),
     path('Equipos/ingresar',views.ingresar,name='ingresar'),
+    #CRUD equipos
     path('Equipos',views.Equipos,name='Equipos'), 
     path('Equipos/editar/<int:id>',views.editar,name='editar'),
     path('borrar/<int:id>' , views.borrar, name='borrar'),
     path('Equipos/editar/<int:id>/',views.editar,name='editar'),
     path('borrar/<int:id>/' , views.borrar, name='borrar'),
     path('Equipos/ver/<int:id>/', views.ver, name='ver'),
+
+    #CRUD responsables
     path('Responsables' , views.Responsables, name='Responsables'),
     path('Responsables/crear' , views.Responsablescrear, name='Responsablescrear'),
-    
+    path('Responsables/editarResponsable/<int:id>',views.editarResponsable, name='editarResponsable'),
+    path('borrarResponsable/<int:id>/' , views.borrarResponsable, name='borrarResponsable'),
+
     
     #secciones
     path('Equipos/biomedica', views.Biomedica, name='Biomedica'),
--- Hospital/hce/views.py
+++ Hospital/hce/views.py
@@ -14,51 +14,19 @@ def equipo (request):
 def responsable (request):
     return render (request, 'Responsables/responsable.html')
 
-def ingresar (request): #análogo a crear
+#CRUD PARA EQUIPOS
+def Equipos(request):
+    equipo = Equipo.objects.all() #trae el objeto con toda su informacion
+    return render(request , 'Equipos/index.html', {'equipos':equipo}) #captura la información desde la base de datos
+
+
+def ingresar (request):
      infoEquipo=EquipoForm(request.POST, request.FILES or None)
      asignarResponsable=Responsable.objects.all()
      if infoEquipo.is_valid():
           infoEquipo.save()
           return redirect('Equipos')
      return render(request,'Equipos/ingresar.html',{'Responsables':asignarResponsable})
-
-def Equipos(request):
-    equipo = Equipo.objects.all() #trae el objeto con toda su informacion
-    return render(request , 'Equipos/index.html', {'equipos':equipo}) #captura la información desde la base de datos
-
-#secciones para cada área de equipos
-
-def Biomedica(request):
-    equipo = Equipo.objects.all() #trae el objeto con toda su informacion
-    return render(request , 'Equipos/biomedica.html', {'equipos':equipo}) #captura la información desde la base de datos
-def Infraestructura(request):
-    equipo = Equipo.objects.all() #trae el objeto con toda su informacion
-    return render(request , 'Equipos/infraestructura.html', {'equipos':equipo}) #captura la información desde la base de datos
-def Sistemas(request):
-    equipo = Equipo.objects.all() #trae el objeto con toda su informacion
-    return render(request , 'Equipos/sistemas.html', {'equipos':equipo}) #captura la información desde la base de datos
-
-
-def Responsables(request):
-     asignarResponsable = Responsable.objects.all() 
-     return render(request,'Responsables/index.html',{'Responsables':asignarResponsable})
-
-def Responsablescrear(request):
-    infoEquipo = ResponsableForm(request.POST or None)
-    if infoEquipo.is_valid():
-        infoEquipo.save()
-        return redirect('Responsables')
-    else:
-        print(infoEquipo.errors)  # Muestra errores del formulario para depuración
-    return render(request, 'Responsables/crear.html', {'form': infoEquipo})
-
-
-
-
-def borrar(request, id):
-    equipo = Equipo.objects.get(id=id)
-    equipo.delete()
-    return redirect('Equipos')
 
 def editar(request, id):
     equipo = Equipo.objects.get(id=id)
@@ -80,4 +48,57 @@ def editar(request, id):
 def ver (request,id):
     equipo = Equipo.objects.get(id=id) #Busca el Id
     return render(request, 'Equipos/ver.html', {'equipo': equipo})
+
+def borrar(request, id):
+    equipo = Equipo.objects.get(id=id)
+    equipo.delete()
+    return redirect('Equipos')
+
+
+#secciones para cada área de equipos
+
+def Biomedica(request):
+    equipo = Equipo.objects.all() #trae el objeto con toda su informacion
+    return render(request , 'Equipos/biomedica.html', {'equipos':equipo}) #captura la información desde la base de datos
+def Infraestructura(request):
+    equipo = Equipo.objects.all() #trae el objeto con toda su informacion
+    return render(request , 'Equipos/infraestructura.html', {'equipos':equipo}) #captura la información desde la base de datos
+def Sistemas(request):
+    equipo = Equipo.objects.all() #trae el objeto con toda su informacion
+    return render(request , 'Equipos/sistemas.html', {'equipos':equipo}) #captura la información desde la base de datos
+
+
+#CRUD para responsables
+
+def Responsables(request):
+     asignarResponsable = Responsable.objects.all() 
+     return render(request,'Responsables/index.html',{'Responsables':asignarResponsable})
+
+def Responsablescrear(request):
+    infoEquipo = ResponsableForm(request.POST or None)
+    if infoEquipo.is_valid():
+        infoEquipo.save()
+        return redirect('Responsables')
+    else:
+        print(infoEquipo.errors)  # Muestra errores del formulario para depuración
+    return render(request, 'Responsables/crear.html', {'form': infoEquipo})
+
+def editarResponsable(request, id):
+    responsable = Responsable.objects.get(id=id)
+    formResponsable = ResponsableForm(request.POST or None, request.FILES or None, instance=responsable)
+
+    if formResponsable.is_valid() and request.method == 'POST':
+        formResponsable.save()
+        return redirect('Responsables')
+
+    return render(request,'Responsables/editarResponsable.html', {
+        'formResponsable': formResponsable,
+        
+    })
+
+
+def borrarResponsable(request, id):
+    responsable = Responsable.objects.get(id=id)
+    responsable.delete()
+    return redirect('Responsables')
 
